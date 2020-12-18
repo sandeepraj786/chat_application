7f196177819d2e2a6699ad0c6868f6ae18f58ca4
root <root@ip-10-0-1-188.us-east-2.compute.internal>
2020-12-18 06:38:55 +0000

    ADDED: scripts folder and aapspec file

---
+++ scripts/django.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+sudo systemctl daemon-reload sudo systemctl start django sudo systemctl 
+enable djangosudo systemctl daemon-reload sudo systemctl start django
+sudo systemctl enable django
